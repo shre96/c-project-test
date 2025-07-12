e8d60df317c8540e9102d82c82738b212357bfb5
abc <abc>
2025-07-12 06:38:18 +0000

    added c files

---
+++ main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+void biggest3();    // function declared
+void factorial();   // function declared
+void palindrome();  // function declared
+
+int main() {
+    biggest3();
+    factorial();
+    palindrome();
+    return 0;
+}
+
